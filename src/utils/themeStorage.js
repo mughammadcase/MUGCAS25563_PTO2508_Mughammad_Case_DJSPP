ee5e5e037680edc0c9c0c3a17d14978e04172098
Mughammad <caseameen44@gmail.com>
2026-07-15 22:12:28 +0200

    Add themeStorage utility
    - Reuse localStorage logic and architecture from JSLPP
    
    - Utility is responsible for persisting data

---
+++ src/utils/themeStorage.js
@@ -0,0 +1,13 @@
+/**
+ * Retrieves the saved theme from localStorage
+ * Default to light mode if no theme is saved
+ *
+ * returns {string} The saved theme: light or dark
+ */
+export function getStoredTheme() {
+  return localStorage.getItem("theme") || "light";
+}
+
+export function saveTheme(theme) {
+  localStorage.getItem("theme", theme);
+}
